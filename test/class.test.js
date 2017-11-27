a8b7dec9ff38aa5323eee9184b7825147d171efb
DogJun <498343874@qq.com>
2017-11-27 14:13:34 +0800

    新增自动化测试

--- test/class.test.js
+++ test/class.test.js
@@ -3,9 +3,50 @@ describe('Class API:', function () {
     let $ele = null
     before(function () {
       let div = document.createElement('div')
-      div.id = 'J_addClass'
+      div.id = 'addClass-hook'
       document.body.appendChild(div)
-      $ele = document.querySelector('#J_addClass')
+      $ele = document.querySelector('#addClass-hook')
+    })
+    it(`outils.addClass($ele, 'test') should return true`, function () {
+      outils.addClass($ele, 'test')
+      assert(outils.hasClass($ele, 'test'))
+    })
+    after(function () {
+      document.body.removeChild($ele)
+    })
+  })
+
+  describe('#hasClass()', function () {
+    let $ele = null
+    before(function () {
+      let div = document.createElement('div')
+      div.id = 'hasClass-hook'
+      document.body.appendChild(div)
+      $ele = document.querySelector('#hasClass-hook')
+    })
+    it(`outils.hasClass($ele, 'test') should return true`, function () {
+      outils.hasClass($ele, 'test')
+      assert(outils.hasClass($ele, 'test'))
+    })
+    after(function () {
+      document.body.removeChild($ele)
+    })
+  })
+
+  describe('#removeClass()', function () {
+    let $ele = null
+    before(function () {
+      let div = document.createElement('div')
+      div.id = 'removeClass-hook'
+      document.body.appendChild(div)
+      $ele = document.querySelector('#removeClass-hook')
+    })
+    it(`outils.removeClass($ele, 'test') should return false`, function () {
+      outils.removeClass($ele, 'test')
+      assert.notEqual(outils.hasClass($ele, 'test'))
+    })
+    after(function () {
+      document.body.removeChild($ele)
     })
   })
 })
